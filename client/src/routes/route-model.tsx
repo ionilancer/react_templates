095d46384a7289a5bf67a6c1fa7bee32c8b477df
ionilancer <ionilancer@gmail.com>
2022-12-03 15:04:15 +0100

    chore: create dfault route refactor

--- client/src/routes/route-model.tsx
+++ client/src/routes/route-model.tsx
@@ -1,55 +1,21 @@
 import { Route } from "react-router-dom";
 import { Home, TemplatePage, SorryPage } from "../pages";
 
-export type routerTypeKey = JSX.Element;
-interface ControlsMap {
-  [key: string]: routerTypeKey;
+interface RoutesCmponentModel {
+  [key: string]: JSX.Element;
 }
 
-export const elements2: ControlsMap = {
+export const routeEnumToCompone: RoutesCmponentModel = {
   "/": <Home />,
   "/template/:templateCode": <TemplatePage />,
   "/sorry": <SorryPage />,
 };
 
 export const createRouteDefault = () => {
-  return Object.keys(elements2).map(
+  return Object.keys(routeEnumToCompone).map(
     (key: string) =>
       (
-        <Route path={key} element={elements2[key]} key={key} />
+        <Route path={key} element={routeEnumToCompone[key]} key={key} />
       ) as React.ReactElement
   );
-};
-
-/**
- * with T
- */
-const enum routerPathEnum {
-  home = "/",
-  template = "/template/:templateCode",
-  sorry = "/sorry",
-}
-interface IRoute<T> {
-  path: routerPathEnum;
-  element: T;
-}
-
-export const defaultRoutes: IRoute<routerTypeKey>[] = [
-  {
-    path: routerPathEnum.home,
-    element: <Home />,
-  },
-  {
-    path: routerPathEnum.template,
-    element: <TemplatePage />,
-  },
-  {
-    path: routerPathEnum.sorry,
-    element: <SorryPage />,
-  },
-];
-export const createRoute = () => {
-  return defaultRoutes.map((router) => (
-    <Route path={router.path} element={router.element} key={router.path} />
-  ));
 };
