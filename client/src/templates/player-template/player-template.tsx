436768b03a0a813207a9f3a9721db2881a67d68a
ionilancer <ionilancer@gmail.com>
2022-12-04 21:05:39 +0100

    chore: music player styles

--- client/src/templates/player-template/player-template.tsx
+++ client/src/templates/player-template/player-template.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { PlayerTemplateModel } from "./model";
-import { MusicPlayer } from "../../features/music-player";
 import "./player-template.css";
 import { VideoPlayer } from "../../features/background-video-player";
+import MusicPlayerModal from "../../features/music-player-modal/music-player-modal";
 
 export const PlayerTemplate = (model: PlayerTemplateModel) => {
   return (
     <>
       <VideoPlayer video={model.songList[0].video} />
-      <MusicPlayer {...model.songList[0]} />
+      <MusicPlayerModal />
     </>
   );
 };
